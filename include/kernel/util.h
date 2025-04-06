81eeb310fe78541ed06c9e2575fde81b74b9b383
FlectherTian <1365287231@qq.com>
2025-04-06 13:26:25 +0800

    到了一个能编译的状态

--- include/kernel/util.h
+++ include/kernel/util.h
@@ -5,7 +5,7 @@
 #include <kernel/util/qstr.h>
 #include <kernel/util/radix_tree.h>
 #include <kernel/util/spinlock.h>
-//#include <kernel/util/string.h>
+#include <kernel/util/string.h>
 #include <kernel/util/sync_utils.h>
 #include <kernel/util/misc.h>
 #include <kernel/util/print.h>
